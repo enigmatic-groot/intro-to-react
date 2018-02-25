be78e664f1b9ff8f239c3221dc1355a20699a9dc
Jeremiah Judkins <jeremiahjudkins@Jeremiah-Judkinss-iMac.local>
2018-02-24 18:29:16 -0800

    Assigment-8-ReactT0-D0-Delete---Events-

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
+  deleteToDo(description) {
+    this.props.deleteToDo(description);
+  }
    render() {
      return (
+       <div className="wrapper">
+      {/*Add a delete button to the ToDo component with onClick event listener*/}
+       <button className="deleteToDo" onClick = {(e) =>
+       this.deleteToDo(this.props.description)}>Delete</button>
+       {() => this.props.deleteToDo(this.props.description)}
+
        <li>                                                      {/* Event listener calls Anonymous function and Anonymous function pasess the index to modify*/}
          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
          </li>
+       </div>
      );
    }
  }
